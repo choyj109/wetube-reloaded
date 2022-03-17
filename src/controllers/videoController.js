406184ee2e8d7c586ebfdcfe85c59faa417153ef
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-17 21:19:18 +0900

    #5.4 Variables
    #{variable} in pug templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,13 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => {
-  return res.render("edit");
+  return res.render("edit", { pageTitle: "Edit" });
 };
 export const search = (req, res) => {
-  return res.render("search");
+  return res.render("search", { pageTitle: "Search" });
 };
 export const upload = (req, res) => {
-  return res.render("upload");
+  return res.render("upload", { pageTitle: "Search" });
 };
 export const deleteVideo = (req, res) => {
   return res.render("delete-video");
