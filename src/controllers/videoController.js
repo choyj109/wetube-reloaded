e38cc6e5b6c95ec02111ea36059f4c27c05b1715
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-17 21:42:43 +0900

    #5.8 iteration
    controller에서 const videos = []
    each 변수 in array
       li=변수

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,17 @@
-const fakeUser = {
-  username: "Yoonjoo",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+    },
+    {
+      title: "Video #2",
+    },
+    {
+      title: "What's up",
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => {
   return res.render("edit", { pageTitle: "Edit" });
