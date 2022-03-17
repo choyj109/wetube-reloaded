6d52f2cdc8a5f4c95c56af497ede22e10c35ddb4
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-17 18:08:02 +0900

    #3.6 Middleware
    make middleware
    app.use(middleware)

--- src/server.js
+++ src/server.js
@@ -4,21 +4,37 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
 
-const handelHome = (req, res) => {
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("Not allowed");
+  }
+  console.log("Allowed, you may continue");
+  next();
+};
+
+const handleHome = (req, res) => {
   return res.send("Home");
 };
 
-const handelLogin = (req, res) => {
+const handleLogin = (req, res) => {
   return res.send("Login");
 };
 
-app.get("/", gossipMiddleware, handelHome);
-app.get("/login", handelLogin);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
