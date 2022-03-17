873f8de585c2c3beb1b228ecbd844b8e328142cc
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-17 13:24:09 +0900

    #3.4 App.get
    add app.get

--- src/server.js
+++ src/server.js
@@ -4,7 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/");
+const handelHome = (req, res) => {
+  return res.send("Home");
+};
+
+const handelLogin = (req, res) => {
+  return res.send("Login");
+};
+
+app.get("/", handelHome);
+app.get("/login", handelLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
