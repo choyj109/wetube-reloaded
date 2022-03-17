022e0211e97a3a79dde75df4169f6269d258f99d
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-17 18:19:19 +0900

    #3.11 External
    npm i morgan
    import morgan

--- src/server.js
+++ src/server.js
@@ -1,40 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("Not allowed");
-  }
-  console.log("Allowed, you may continue");
-  next();
-};
-
-const handleHome = (req, res) => {
+const home = (req, res) => {
   return res.send("Home");
 };
 
-const handleLogin = (req, res) => {
+const login = (req, res) => {
   return res.send("Login");
 };
 
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge");
-};
-
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
