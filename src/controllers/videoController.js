7a1db1d5eba7780b649e61380ca0a14d8fd975fd
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-17 21:35:34 +0900

    #5.7 conditionals
    make 가짜 객체 object
    if else
    controller 함수 {변수 자리}

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,10 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "Yoonjoo",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => {
   return res.render("edit", { pageTitle: "Edit" });
