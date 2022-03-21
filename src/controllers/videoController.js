e0dfedd92e760bc875189f5c55bd51e84cdcee3b
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-21 18:52:00 +0900

    #6.0 Array database
    add id in array
    add href on mixin
    const id = req.params

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,30 +1,38 @@
+let videos = [
+  {
+    title: "First",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-    {
-      title: "Second",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-    {
-      title: "Third",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => {
   return res.render("edit", { pageTitle: "Edit" });
 };
