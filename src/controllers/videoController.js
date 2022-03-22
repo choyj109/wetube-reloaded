2023421f83dbd50403db73702889d3cbce7d1961
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-22 20:02:16 +0900

    #6.5,6 more practice
    just practice with fiction data

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -47,8 +47,21 @@ export const postEdit = (req, res) => {
 export const search = (req, res) => {
   return res.render("search", { pageTitle: "Search" });
 };
-export const upload = (req, res) => {
-  return res.render("upload", { pageTitle: "Search" });
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 5,
+    comments: 2,
+    createdAt: "just now",
+    views: 59,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
 export const deleteVideo = (req, res) => {
   return res.render("delete-video");
