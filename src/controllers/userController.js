c48b7f79f4d8782c2bc4bff463a8dd829d973faf
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-03-24 15:26:35 +0900

    #7.4 Status code
    res.status(400)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,14 +5,14 @@ export const postJoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
   if (password !== password2) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match",
     });
   }
   const exist = await User.exists({ $or: [{ username }, { email }] });
   if (exist) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "This username/email is already taken",
     });
