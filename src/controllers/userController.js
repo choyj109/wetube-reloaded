decb0fc6d0f619b9446c98051f5ef59b1f7f8a32
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-04-02 10:49:18 +0900

    #7.19 Github Login
    npm i node-fetch@2.6.1
    import in controller
    application/json
    notification
    await(await fatch()).json()

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -72,13 +73,26 @@ export const finishGithubLogin = async (req, res) => {
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
-  const data = await fetch(finalUrl, {
-    method: "POST",
-    headers: {
-      Accept: "application / json",
-    },
-  });
-  const json = await data.json();
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `${access_token}`,
+        },
+      })
+    ).json();
+  } else {
+    return res.rendirect("/login");
+  }
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
