0513bab05f448397b35ee647093a2e6763bab10b
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-04-05 16:51:38 +0900

    #8.0 Edit profile
    export getEdit postEdit
    userRouter.route().get().post()
    add edit-profile pug
    when loggedInUser undefined
    - req.session.user || {}

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -123,9 +123,14 @@ export const finishGithubLogin = async (req, res) => {
     return res.redirect("/login");
   }
 };
-export const edit = (req, res) => res.send("Edit User");
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
+};
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
 export const see = (req, res) => res.send("See User");
