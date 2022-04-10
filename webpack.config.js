fb606bc45b4a6dbc0c029c42358fe95745c71ed2
choyj109 <101330477+choyj109@users.noreply.github.com>
2022-04-10 13:58:45 +0900

    #9.2 webpack configuration
    import path
    path.resolve(__dirname, "file", "js")
    npm run assets
    npm i babel-loader
    remember entry~rules
    mode: "development"

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
+const path = require("path");
+
 module.exports = {
   entry: "./src/client/js/main.js",
   output: {
     filename: "main.js",
-    path: "./assets/js",
+    path: path.resolve(__dirname, "assets", "js"),
+  },
+  module: {
+    loader: "babel-loader",
+    mode: "development",
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
