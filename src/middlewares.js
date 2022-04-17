acfc324e69a647b602fef1e84848285b03332f53
choyj109 <choyj109@gmail.com>
2022-04-17 21:49:38 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,18 +2,18 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
 
-const s3 = new.aws.S3({
+const s3 = new aws.S3({
   Credential: {
     accessKeyId: process.env.AWS_ID,
-    secretAccessKey: process.env.AWS_SECRET
-  }
-})
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
 
 const multerUploader = multerS3({
   s3: s3,
-  bucket: 'wetube-bucket',
-  acl: 'public-read',
-})
+  bucket: "wetube-bucket",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
